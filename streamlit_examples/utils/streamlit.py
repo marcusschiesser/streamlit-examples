1df6c04b6641c3206c1d6ccd16ea8335213cc862
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2023-10-12 03:29:14 +0000

    feat: add header banner

--- streamlit_examples/utils/streamlit.py
+++ streamlit_examples/utils/streamlit.py
@@ -60,8 +60,41 @@ def icon(icon_name):
 
 def initPage(page_title):
     dir_root = os.path.dirname(os.path.abspath(__file__))
-    logo = Image.open(dir_root+'/../assets/logo.png')
     favicon = Image.open(dir_root+'/../assets/favicon.png')
     st.set_page_config(page_title=page_title, page_icon=favicon)
     local_css("style.css")
-    st.sidebar.image(logo)
+    # insert header brand with logo and link to website 
+    st.components.v1.html(
+        """
+        <style>
+            .header-brand {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+            }
+        </style>
+
+        <div class="header-brand">
+            <a href="https://schiesser-it.com/">
+                <img src="https://schiesser-it.com/images/logo/schiesser.svg" width="200" />
+            </a>
+            <a href="https://www.linkedin.com/in/marcusschiesser">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="#0a66c2"
+                    class="mercado-match"
+                    width="32"
+                    height="32"
+                    focusable="false"
+                >
+                    <path
+                        d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"
+                    ></path>
+                </svg>
+            </a>
+        </div>
+        """,
+        height=50,
+    )
+    st.divider()
